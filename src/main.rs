9de506d6984e9fa1d0dd2e9dc8913a9a877f55aa
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-04-10 10:09:19 +0200

    retourne 404 si besoin

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,46 @@ fn gestion_conneexion(mut flux: TcpStream) {
 
     flux.read(&mut tampon).unwrap();
 
-    println!("Requête : {}", String::from_utf8_lossy(&tampon[..]));
 
-    let contenu = fs::read_to_string("example.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let reponse = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contenu.len(),
-        contenu
-    );
+    if tampon.starts_with(get) {
+        println!("Requête : {}", String::from_utf8_lossy(&tampon[..]));
 
-    flux.write(reponse.as_bytes()).unwrap();
-    flux.flush().unwrap();
+        let contenu = fs::read_to_string("example.html").unwrap();
+
+        let reponse = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contenu.len(),
+            contenu
+        );
+        flux.write(reponse.as_bytes()).unwrap();
+        flux.flush().unwrap();
+
+    } else if tampon.starts_with(b"GET /favicon.ico HTTP/1.1\r\n") {
+        let contenu = fs::read_to_string("favicon.ico").unwrap();
+
+        let reponse = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contenu.len(),
+            contenu
+        );
+        flux.write(reponse.as_bytes()).unwrap();
+        flux.flush().unwrap();
+
+    } else {
+        let ligne_statut = "HTTP/1.1 404 NOT FOUND";
+        let contenu = fs::read_to_string("404.html").unwrap();
+        let reponse = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            ligne_statut,
+            contenu.len(),
+            contenu
+        );
+        flux.write(reponse.as_bytes()).unwrap();
+        flux.flush().unwrap();
+
+
+    }
+    
 }
