63f7dd3b3365949e5add13e90ed09b49e8c12c04
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-04-23 10:09:59 +0200

    requete ogn

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::net::TcpStream;
+use chrono::{DateTime, NaiveDate, StrftimeItems, prelude::*};
+
 struct Vol {
     numero: i32,
     planeur: String,
@@ -10,4 +13,14 @@ struct Vol {
     minute_decolage: u8,
     heure_atterissage: u8,
     minute_atterissage: u8,
+}
+
+async fn requete_ogn() -> String {
+    let jour = 23;
+    let mois = 04;
+    let annee = 2023;
+    let airfield_code = "LFLE";
+    let reponse = reqwest::get(format!("http://flightbook.glidernet.org/api/logbook/{}/{}-{}-{}", airfield_code, annee, mois, jour)).await?;
+    let corps = reponse.text().await?;
+    corps
 }
