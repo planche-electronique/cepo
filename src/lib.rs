3fef7824e9e314800481ec6a0e479e3e2e74ae6d
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-04-23 09:16:47 +0200

    structure d'un vol

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+struct Vol {
+    numero: i32,
+    planeur: String,
+    pilote_1: String,
+    pilote_2: String,
+    code_vol: String,
+    code_deco: String,
+    machine_deco: String,
+    heure_decolage: u8,
+    minute_decolage: u8,
+    heure_atterissage: u8,
+    minute_atterissage: u8,
+}
