b0fc87aec495b82ce27ccd66c4001f4566cf772f
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-04-23 10:59:38 +0200

    fonction qui permet de lire une liste d'immatriculations

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::net::TcpStream;
-use chrono::{DateTime, NaiveDate, StrftimeItems, prelude::*};
+use std::fs;
+use json::JsonValue::Array;
 
 struct Vol {
     numero: i32,
@@ -20,7 +20,34 @@ async fn requete_ogn() -> String {
     let mois = 04;
     let annee = 2023;
     let airfield_code = "LFLE";
-    let reponse = reqwest::get(format!("http://flightbook.glidernet.org/api/logbook/{}/{}-{}-{}", airfield_code, annee, mois, jour)).await?;
-    let corps = reponse.text().await?;
+    let reponse = reqwest::get(format!("http://flightbook.glidernet.org/api/logbook/{}/{}-{}-{}", airfield_code, annee, mois, jour)).await.unwrap();
+    let corps = reponse.text().await.unwrap();
     corps
+}
+
+fn liste_immatriculations() -> Vec<String> {
+    let contenu_fichier = fs::read_to_string("immatriculations.json")
+        .expect("Probleme lors de la leture du fichier");
+    let fichier_parse = json::parse(contenu_fichier.as_str()).unwrap();
+    let immatriculations_json = match fichier_parse {   
+        Array(vecteur) => {
+            vecteur
+        },
+        _ => {
+            eprintln!("immatriculations.json n'est pas un tableau");
+            Vec::new()
+        },
+    };
+    let mut immatriculations = Vec::new();
+    for immatriculation_json in immatriculations_json {
+        match immatriculation_json {
+            json::JsonValue::String(immatriculation) => {
+                immatriculations.push(immatriculation);
+            },
+            _ => {
+                eprintln!("{} n'est pas de type string", immatriculation_json);
+            }
+        }
+    }
+    immatriculations
 }
