9ddab73baa1bec9ed972d021f8bff40fd3eaf7a5
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-04-23 16:20:03 +0200

    fonction pour ecrire les fichiers si nouveaux, en cours

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,15 @@
 use std::fs;
-use json::JsonValue::{Short, Null, Number, Boolean, Object, Array};
+use json::JsonValue::Array;
 use chrono::prelude::*;
 
 
 struct Vol {
     numero: i32,
     planeur: String,
-    pilote_1: String,
-    pilote_2: String,
-    code_vol: String,
-    code_deco: String,
-    machine_deco: String,
-    heure_decolage: u8,
-    minute_decolage: u8,
-    heure_atterissage: u8,
-    minute_atterissage: u8,
+    decollage: NaiveTime,
+    atterissage: NaiveTime,
 }
 
-#[derive(Clone)]
 struct Appareil {
     modele: String,
     categorie: u8,
@@ -43,6 +35,8 @@ pub fn traitement_requete_ogn(date: NaiveDate, requete: String) {
             Vec::new()
         },
     };
+
+
     for appareil in tableau_devices {
         let modele_json = appareil["aircraft"].clone();
         let modele = modele_json.as_str().unwrap().to_string();
@@ -61,25 +55,83 @@ pub fn traitement_requete_ogn(date: NaiveDate, requete: String) {
         };
         appareils_ogn.push(appareil_actuel);
     }
-    //on ne garde que les appareils de la liste d'immatriculations
-    let mut appareils_cibles: Vec<Appareil> = Vec::new();
-    for immatriculation in liste_immatriculations() {
-        for appareil in &appareils_ogn {
-            if immatriculation == appareil.immatriculation {
-                appareils_cibles.push((*appareil).clone());
-                println!("{}", immatriculation);
-            }
+
+
+    let mut vols: Vec<Vol> = Vec::new();
+    let flights = requete_parse["flights"].clone();
+    let vols_json = match flights {
+        Array(vols_json) => vols_json,
+        _ => {
+            eprintln!("n'est pas un tableau");
+            Vec::new()
+        }
+    };
+    let mut index = 1;
+    for vol_json in vols_json {
+        let mut start_json = vol_json["start"].clone();
+        let start_str = start_json.take_string().unwrap().clone();
+        let decollage = NaiveTime::parse_from_str(format!("{}",start_str).as_str(), "%Hh%M").unwrap();
+        
+        let mut stop_json = vol_json["stop"].clone();
+        let stop_str = stop_json.take_string().unwrap();
+        let atterissage = NaiveTime::parse_from_str(format!("{}",stop_str).as_str(), "%Hh%M").unwrap();
+
+        let device = vol_json["device"].clone();
+        let device_number = device.as_u8().unwrap() as usize;
+        let immatriculation = appareils_ogn[device_number].immatriculation.clone();
+        
+        vols.push( Vol {
+            numero: index,
+            planeur: immatriculation,
+            decollage: decollage,
+            atterissage: atterissage,
+        });
+        index += 1;
+    }
+}
+
+fn enregistrer_vols() {
+    let mut chemins = fs::read_dir("./").unwrap();
+    let maintenant = Utc::now();
+    let annee = maintenant.date_naive().format("%Y").to_string();
+    let mut annee_existe = false;
+    for chemin in chemins {
+        if chemin.unwrap().path().to_str().unwrap().to_string() == annee {
+            annee_existe = true;
         }
     }
+    if annee_existe == false {
+        fs::create_dir(format!("./{}", annee)).unwrap();
+    }
 
-    //on crée un vecteur de struct Vol
-    /* on itere sur le tableau de vols (nommé "flights") retourné par l'api
-    infos utiles:
-    "start"
-    "stop"
-    "device"
-    "towing" auquel ca "tow" aussi
-    */
+    chemins = fs::read_dir("./").unwrap();
+    let mois = maintenant.date_naive().format("%m").to_string();
+    let mut mois_existe = false;
+    for chemin in chemins {
+        if chemin.unwrap().path().to_str().unwrap().to_string() == annee {
+            mois_existe = true;
+        }
+    }
+    if mois_existe == false {
+        fs::create_dir(format!("./{}", mois)).unwrap();
+    }
+
+    chemins = fs::read_dir("./").unwrap();
+    let jour = maintenant.date_naive().format("%d").to_string();
+    let mut jour_existe = false;
+    for chemin in chemins {
+        if chemin.unwrap().path().to_str().unwrap().to_string() == annee {
+            jour_existe = true;
+        }
+    }
+    if jour_existe == false {
+        fs::create_dir(format!("./{}", jour)).unwrap();
+    }
+
+    //on cree une liste de string json des vols
+
+    //on itere sur ce vecteur, puis on verifie l'egalite le dossier du jour voulu, 
+
 }
 
 fn liste_immatriculations() -> Vec<String> {
--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,9 @@
 mod tests {
-    use chrono::prelude::*;
-    use db_interaction_server::requete_ogn;
     #[test]
     fn test_traitement_requete_ogn() {
+        use chrono::prelude::*;
+        use db_interaction_server::requete_ogn;
+
         let date = NaiveDate::from_ymd_opt(2023, 04, 21).unwrap();
         assert_eq!(requete_ogn(date), "{\"a_day\":\"Fri\",\"airfield\":{\"code\":\"LFLE\",\"country\":\"FR\",\"elevation\":297,\"latlng\":[45.56055,5.97584],\"name\":\"Chambery Challes les Eaux\",\"time_info\":{\"dawn\":\"06h09\",\"noon\":\"13h35\",\"sunrise\":\"06h41\",\"sunset\":\"20h30\",\"twilight\":\"21h01\",\"tz_name\":\"Europe/Paris\",\"tz_offset\":\"CEST+0200\"}},\"call_tsp\":1682244524,\"code\":\"LFLE\",\"date\":\"2023-04-21\",\"devices\":[{\"address\":\"3849F2\",\"aircraft\":\"DR-300\",\"aircraft_type\":2,\"competition\":null,\"db_org\":\"OGN\",\"device_type\":\"F\",\"identified\":true,\"registration\":\"F-BSPS\",\"tracked\":true}],\"flights\":[{\"device\":0,\"duration\":2208,\"max_alt\":1318,\"max_height\":1021,\"start\":\"16h32\",\"start_q\":32,\"start_tsp\":1682087575,\"stop\":\"17h09\",\"stop_q\":32,\"stop_tsp\":1682089783,\"tow\":null,\"towing\":false,\"warn\":false}],\"rnames\":[\"LFLE\"]}\n".to_string())
     }
