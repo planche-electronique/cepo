1ea1a3fc9143ad2adb839850468e2e450963a754
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-04-28 18:46:03 +0200

    ajout d'une fonction pour lire les pilotes

---
+++ src/pilotes.rs
@@ -0,0 +1,29 @@
+use json;
+
+fn lire_liste_pilotes() -> Vec<String> {
+    let contenu = fs::read_to_string("./parametres/pilotes.json").unwrap_or_default();
+    let fichier_parse = json::parse(contenu_fichier.as_str()).unwrap();
+    let pilotes_json = match fichier_parse {   
+        Array(vecteur) => {
+            vecteur
+        },
+        _ => {
+            eprintln!("pilotes.json n'est pas un tableau");
+            Vec::new()
+        },
+    };
+
+    let mut pilotes = Vec::new();
+
+    for pilote_json in pilotes_json {
+        match pilote_json {
+            json::JsonValue::Short(pilote) => {
+                pilotes.push(pilote.as_str().to_string());
+            },
+            _ => {
+                eprintln!("{} n'est pas de type short", pilote_json);
+            }
+        }
+    }
+    vols
+}
