0c13852d8bce65c8fa499fdc1deb70d1c1af8ff1
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-06-22 14:10:24 +0200

    creation d'un objet planche qui a une date
    - suppression du fichier mise a jour maintenant dans celui de planche
    - fonction enregistrer vols maintenant dans l'implementation de planche
    - fonction creer_chemin_jour maintenant dans lib.rs pour faciliter son
      utilisation dans tout le programme
    - création d'une fonction qui renvoie une planche d'après lecture

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,11 @@ use std::fs;
 pub use crate::vol::{Vol, VolJson};
 mod vol;
 
-pub use crate::client::{VariationRequete, Client};
+pub use crate::client::{Client, VariationRequete};
 mod client;
 
-pub use crate::mise_a_jour::{MiseAJour, MettreAJour};
-mod mise_a_jour;
-
+pub use crate::planche::{MettreAJour, MiseAJour, Planche};
+mod planche;
 
 pub struct Appareil {
     pub modele: String,
@@ -29,13 +28,62 @@ pub fn liste_immatriculations() -> Vec<String> {
     immatriculations
 }
 
-
 pub fn nom_fichier_date(nombre: i32) -> String {
     let nombre_str: String;
     if nombre > 9 {
         nombre_str = nombre.to_string();
     } else {
         nombre_str = format!("0{}", nombre.to_string());
-    } 
+    }
     nombre_str
+}
+
+pub fn creer_chemin_jour(annee: i32, mois: u32, jour: u32) {
+    let jour_str = nom_fichier_date(jour as i32);
+    let mois_str = nom_fichier_date(mois as i32);
+
+    let chemins = fs::read_dir("./dossier_de_travail").unwrap();
+    let mut annee_existe = false;
+    for chemin in chemins {
+        let chemin_dossier = chemin.unwrap().path().to_str().unwrap().to_string();
+        if chemin_dossier == format!("./dossier_de_travail\\{}", annee) {
+            annee_existe = true;
+        }
+    }
+    if annee_existe == false {
+        fs::create_dir(format!("./dossier_de_travail\\{}", annee)).unwrap();
+    }
+
+    let chemins = fs::read_dir(format!("./dossier_de_travail\\{}", annee)).unwrap();
+    let mut mois_existe = false;
+    for chemin in chemins {
+        let chemin_dossier = chemin.unwrap().path().to_str().unwrap().to_string();
+        if chemin_dossier == format!("./dossier_de_travail\\{}\\{}", annee, mois_str) {
+            mois_existe = true;
+        }
+    }
+    if mois_existe == false {
+        fs::create_dir(format!("./dossier_de_travail\\{}\\{}", annee, mois_str)).unwrap();
+    }
+
+    let chemins = fs::read_dir(format!("./dossier_de_travail\\{}\\{}", annee, mois_str)).unwrap();
+    let mut jour_existe = false;
+    for chemin in chemins {
+        let chemin_dossier = chemin.unwrap().path().to_str().unwrap().to_string();
+        if chemin_dossier
+            == format!(
+                "./dossier_de_travail\\{}\\{}\\{}",
+                annee, mois_str, jour_str
+            )
+        {
+            jour_existe = true;
+        }
+    }
+    if jour_existe == false {
+        fs::create_dir(format!(
+            "./dossier_de_travail\\{}/{}/{}",
+            annee, mois_str, jour_str
+        ))
+        .unwrap();
+    }
 }
---
+++ src/planche.rs
@@ -0,0 +1,275 @@
+use crate::{creer_chemin_jour, nom_fichier_date};
+use chrono::NaiveDate;
+use std::fs;
+
+#[derive(PartialEq, Debug)]
+pub struct Planche {
+    vols: Vec<Vol>,
+    date: NaiveDate,
+}
+
+impl Planche {
+    pub fn planche_du(date: NaiveDate) -> Planche {
+        let annee = date.year();
+        let mois = date.month();
+        let jour = date.day();
+
+        let mois_str = nom_fichier_date(mois as i32);
+        let jour_str = nom_fichier_date(jour as i32);
+
+        let mut vols: Vec<Vol> = Vec::new();
+
+        let fichiers = fs::read_dir(format!(
+            "./dossier_de_travail/{}/{}/{}",
+            annee, mois_str, jour_str
+        ))
+        .unwrap();
+
+        for fichier in fichiers {
+            let vol_json = fs::read_to_string(fichier.unwrap().path().to_str().unwrap()).unwrap();
+            let vol = Vol::depuis_json(json::parse(vol_json.as_str()).unwrap());
+            vols.push(vol);
+        }
+
+        Planche { vols, date }
+    }
+
+    pub fn enregistrer(self) {
+        let date = self.date;
+        let vols = self.vols;
+        let annee = date.year();
+        let mois = date.month();
+        let jour = date.day();
+
+        let jour_str = nom_fichier_date(jour as i32);
+        let mois_str = nom_fichier_date(mois as i32);
+
+        creer_chemin_jour(annee, mois, jour);
+
+        let mut vols_json = Vec::new();
+        for vol in vols {
+            vols_json.push(vol.vers_json());
+        }
+
+        let mut index = 1;
+        for vol_json in vols_json {
+            let index_str = nom_fichier_date(index);
+            let chemin = format!(
+                "./dossier_de_travail/{}/{}/{}/{}.json",
+                annee, mois_str, jour_str, index_str
+            );
+            let fichier = fs::read_to_string(chemin.clone()).unwrap_or_else(|err| {
+                println!(
+                    "fichier numero {} de chemiun {} introuvable ou non ouvrable : {}",
+                    index,
+                    chemin.clone(),
+                    err.to_string()
+                );
+                "".to_string()
+            });
+
+            if fichier != vol_json {
+                fs::write(chemin, vol_json).expect("impossible d'ecrire le fichier");
+            }
+            index += 1;
+        }
+    }
+}
+
+pub trait MettreAJour {
+    fn mettre_a_jour(&mut self, mise_a_jour: MiseAJour);
+}
+
+impl MettreAJour for Planche {
+    // on crée une fonction pour mettre la mise à jour dans le vecteur Vols du jour
+    fn mettre_a_jour(&mut self, mise_a_jour: MiseAJour) {
+        let mut vols = self.vols.clone();
+        if mise_a_jour.date != self.date {
+            eprintln!("Mise a jour impossible: les dates ne sont pas les mêmes !");
+        } else {
+            for vol in &mut vols {
+                if vol.numero_ogn == mise_a_jour.numero_ogn as i32 {
+                    match mise_a_jour.champ_mis_a_jour.clone().as_str() {
+                        "code_decollage" => {
+                            vol.code_decollage = mise_a_jour.nouvelle_valeur.clone()
+                        }
+                        "machine_decollage" => {
+                            vol.machine_decollage = mise_a_jour.nouvelle_valeur.clone()
+                        }
+                        "decolleur" => vol.decolleur = mise_a_jour.nouvelle_valeur.clone(),
+                        "aeronef" => vol.aeronef = mise_a_jour.nouvelle_valeur.clone(),
+                        "code_vol" => vol.code_vol = mise_a_jour.nouvelle_valeur.clone(),
+                        "pilote1" => vol.pilote1 = mise_a_jour.nouvelle_valeur.clone(),
+                        "pilote2" => vol.pilote2 = mise_a_jour.nouvelle_valeur.clone(),
+                        "decollage" => {
+                            vol.decollage = NaiveTime::parse_from_str(
+                                format!("{}", mise_a_jour.nouvelle_valeur.clone()).as_str(),
+                                "%H:%M",
+                            )
+                            .unwrap();
+                        }
+                        "atterissage" => {
+                            vol.atterissage = NaiveTime::parse_from_str(
+                                format!("{}", mise_a_jour.nouvelle_valeur.clone()).as_str(),
+                                "%H:%M",
+                            )
+                            .unwrap();
+                        }
+                        _ => {
+                            eprintln!("Requète de mise a jour mauvaise.");
+                        }
+                    }
+                }
+            }
+        }
+        self.vols = vols.clone();
+    }
+}
+
+use crate::vol::Vol;
+use chrono::prelude::*;
+
+#[derive(Debug, PartialEq)]
+pub struct MiseAJour {
+    numero_ogn: u8,
+    champ_mis_a_jour: String,
+    nouvelle_valeur: String,
+    date: NaiveDate,
+}
+
+impl MiseAJour {
+    pub fn new() -> Self {
+        MiseAJour {
+            numero_ogn: u8::default(), //numero du vol **OGN**
+            champ_mis_a_jour: String::default(),
+            nouvelle_valeur: String::default(),
+            date: NaiveDate::default(),
+        }
+    }
+
+    pub fn parse(&mut self, texte_json: json::JsonValue) -> Result<(), String> {
+        match texte_json {
+            json::JsonValue::Object(objet) => {
+                self.numero_ogn = objet["numero_ogn"].as_u8().unwrap_or_else(|| {
+                    eprintln!("pas de numero de vol dans la requete");
+                    0
+                });
+
+                self.champ_mis_a_jour = objet["champ_mis_a_jour"]
+                    .as_str()
+                    .unwrap_or_else(|| {
+                        eprintln!("pas le bon champ pour la nouvelle valeur");
+                        ""
+                    })
+                    .to_string();
+
+                self.nouvelle_valeur = objet["nouvelle_valeur"]
+                    .as_str()
+                    .unwrap_or_else(|| {
+                        eprintln!("pas la bonne valeur pour la nouvelle valeur");
+                        ""
+                    })
+                    .to_string();
+
+                self.date = NaiveDate::parse_from_str(
+                    objet["date"].as_str().unwrap_or_else(|| {
+                        eprintln!("pas la bonne valeur pour la nouvelle valeur");
+                        ""
+                    }),
+                    "%Y/%m/%d",
+                )
+                .unwrap();
+            }
+            _ => {
+                eprintln!("pas un objet json");
+            }
+        };
+        Ok(())
+    }
+}
+
+mod tests {
+
+    #[test]
+    fn mise_a_jour_parse_test() {
+        use crate::MiseAJour;
+        use chrono::NaiveDate;
+        use core::panic;
+
+        let mise_a_jour_declaree = MiseAJour {
+            numero_ogn: 1,
+            champ_mis_a_jour: String::from("code_vol"),
+            nouvelle_valeur: String::from("M"),
+            date: NaiveDate::from_ymd_opt(2023, 04, 25).unwrap(),
+        };
+
+        let mut mise_a_jour_parse = MiseAJour::new();
+        let _ = mise_a_jour_parse.parse(
+            json::parse(
+                "{ \
+                    \"numero_ogn\": 1, \
+                    \"champ_mis_a_jour\": \"code_vol\", \
+                    \"nouvelle_valeur\": \"M\", \
+                    \"date\":  \"2023/04/25\" \
+                }",
+            )
+            .unwrap_or_else(|err| {
+                panic!("{} : erreur !!", err);
+            }),
+        );
+
+        assert_eq!(mise_a_jour_declaree, mise_a_jour_parse)
+    }
+
+    #[test]
+    fn mettre_a_jour_test() {
+        use crate::vol::Vol;
+        use crate::{MettreAJour, MiseAJour, Planche};
+        use chrono::{NaiveDate, NaiveTime};
+
+        let mut vols = Vec::new();
+        vols.push(Vol {
+            numero_ogn: 1,
+            code_decollage: String::from("T"),
+            machine_decollage: String::from("FREMA"),
+            decolleur: String::from("YDL"),
+            aeronef: String::from("F-CERJ"),
+            code_vol: String::from("S"),
+            pilote1: String::from("Walt Disney"),
+            pilote2: String::default(),
+            decollage: NaiveTime::from_hms_opt(13, 0, 0).unwrap(),
+            atterissage: NaiveTime::from_hms_opt(14, 0, 0).unwrap(),
+        });
+
+        let date = NaiveDate::from_ymd_opt(2023, 04, 25).unwrap();
+
+        let mise_a_jour = MiseAJour {
+            numero_ogn: 1,
+            champ_mis_a_jour: String::from("machine_decollage"),
+            nouvelle_valeur: String::from("LUCIFER"),
+            date: NaiveDate::from_ymd_opt(2023, 04, 25).unwrap(),
+        };
+
+        let mut planche = Planche { vols, date };
+        planche.mettre_a_jour(mise_a_jour);
+
+        let vol_verif = Vol {
+            numero_ogn: 1,
+            code_decollage: String::from("T"),
+            machine_decollage: String::from("LUCIFER"),
+            decolleur: String::from("YDL"),
+            aeronef: String::from("F-CERJ"),
+            code_vol: String::from("S"),
+            pilote1: String::from("Walt Disney"),
+            pilote2: String::default(),
+            decollage: NaiveTime::from_hms_opt(13, 0, 0).unwrap(),
+            atterissage: NaiveTime::from_hms_opt(14, 0, 0).unwrap(),
+        };
+        let vols_verif = vec![vol_verif];
+        let planche_verif = Planche {
+            vols: vols_verif,
+            date: date,
+        };
+        assert_eq!(planche, planche_verif)
+    }
+}
