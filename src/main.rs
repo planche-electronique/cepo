f061d78197a5007f2519f89dc8240f8543f389ab
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2024-01-01 09:52:56 +0100

    set correct name for saving configuration

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         );
         Configuration::default()
     });
-    confy::store("serveur", None, configuration.clone())?;
+    confy::store("cepo", None, configuration.clone())?;
     env_logger::Builder::from_env(
         env_logger::Env::default().default_filter_or(configuration.niveau_log.clone()),
     )
