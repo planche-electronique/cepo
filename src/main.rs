7dccbacd7759b1c2a6557427ec40803057a4fc6c
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2024-08-15 09:29:32 +0200

    use the new "log_level" instead of "niveau_log"

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     }
     //confy::store("cepo", None, configuration.clone())?;
     env_logger::Builder::from_env(
-        env_logger::Env::default().default_filter_or(configuration.niveau_log.clone()),
+        env_logger::Env::default().default_filter_or(&configuration.log_level),
     )
     .init();
 
